782e19413997f13185677b853b75ec5ba8a32c0e
Wanglei <flaboy.cn@gmail.com>
2014-12-09 19:56:17 +0800

    init

---
+++ client.go
@@ -0,0 +1,78 @@
+package drpc
+
+import (
+	"crypto/tls"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net"
+	"net/http"
+	"net/url"
+)
+
+type Client struct {
+	ep *Connection
+}
+
+func NewClient() (c *Client) {
+	c = &Client{}
+	c.ep = newConnection()
+	return c
+}
+
+func (me *Client) Channel() *Channel {
+	if me.ep == nil {
+		return nil
+	}
+	return me.ep.Channel()
+}
+
+func (me *Client) Connect(addr string) (err error) {
+	var (
+		use_ssl bool
+		u       *url.URL
+		c       net.Conn
+		rsp     *http.Response
+		conn    *websocket.Conn
+	)
+
+	u, err = url.Parse(addr)
+	if err != nil {
+		return
+	}
+
+	switch u.Scheme {
+	case "http":
+	case "ws":
+		use_ssl = false
+	case "https":
+	case "wss":
+		use_ssl = true
+	}
+
+	c, err = net.Dial("tcp", u.Host)
+	if err != nil {
+		return
+	}
+
+	if use_ssl {
+		c = tls.Client(c, &tls.Config{InsecureSkipVerify: true})
+	}
+
+	headers := http.Header{}
+	conn, rsp, err = websocket.NewClient(c, u, headers, 1024, 1024)
+	if err != nil {
+		fmt.Println(rsp)
+		return
+	}
+
+	me.ep.conn = conn
+	go me.ep.workloop()
+
+	fmt.Sprintf("todo: ", use_ssl)
+
+	return
+}
+
+func (me *Client) Handle(cmd string, f ApiHandler) {
+	me.ep.handle(cmd, f)
+}
---
+++ server.go
@@ -0,0 +1,58 @@
+package drpc
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+type Server struct {
+	apiHandlers map[string]ApiHandler
+	OnConnect   func(*Connection)
+	OnClose     func(string)
+	Connections map[string]*Connection
+}
+
+func NewServer() *Server {
+	return &Server{
+		apiHandlers: make(map[string]ApiHandler),
+		Connections: make(map[string]*Connection),
+	}
+}
+
+func (me *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	ws, err := websocket.Upgrade(w, req, nil, 1024, 1024)
+	if err != nil {
+		log.Println("websocket-error:", err)
+		return
+	}
+
+	me.wshandler(req, ws)
+}
+
+func (me *Server) wshandler(req *http.Request, ws *websocket.Conn) {
+	ep := newConnection()
+	ep.apiHandlers = me.apiHandlers
+	ep.conn = ws
+
+	id := ep.Id()
+	me.Connections[id] = ep
+	defer me.onClose(id)
+
+	if me.OnConnect != nil {
+		go me.OnConnect(ep)
+	}
+
+	ep.workloop()
+}
+
+func (me *Server) onClose(id string) {
+	delete(me.Connections, id)
+	if me.OnClose != nil {
+		me.OnClose(id)
+	}
+}
+
+func (me *Server) Handle(cmd string, f ApiHandler) {
+	me.apiHandlers[cmd] = f
+}
